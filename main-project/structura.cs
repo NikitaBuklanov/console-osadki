544c426fdca88cc1fa67fd8838bf27c705d810af
NikitaBuklanov <buklanovnik@gmail.com>
2022-01-25 16:55:15 +0300

    Добавлены структуры для хранения данных

---
+++ main-project/structura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_project
+{
+    class structura
+    {
+        public int MAX_FILE_ROWS_COUNT = 100;
+        public int MAX_STRING_SIZE = 200;
+
+        public struct Date
+        {
+            public int day;
+            public int month;
+        }
+
+        public struct Osadki
+        {
+            public double wind_count;
+            public string wind_type;
+        }
+
+    }
+}
