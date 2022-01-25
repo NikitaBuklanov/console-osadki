b1e9231fb562f639bbdcbc88d5af23ed2e17c222
NikitaBuklanov <buklanovnik@gmail.com>
2022-01-25 17:56:23 +0300

    Выполнены сортировки

--- main-project/Program.cs
+++ main-project/Program.cs
@@ -20,6 +20,10 @@ namespace main_project
             int size = FileReader.SizeMas(filename);
             object[,] data_rw = new object[size, 4];
 
+            Console.WriteLine("------ ОСАДКИ ------");
+            Console.WriteLine("Дата | Кол-во | Тип");
+            Console.WriteLine("");
+
             FileReader.Read(filename, data_rw);
 
             for (int i = 0; i < size; i++)
@@ -29,7 +33,46 @@ namespace main_project
                 string count = Convert.ToString(data_rw[i, 2]);
                 string type = Convert.ToString(data_rw[i, 3]);
 
-                Console.WriteLine(day + " " + mounth + " " + count + " " + type);
+                Console.WriteLine(day + "." + mounth + "  |  " + count.PadRight(5, ' ') + " | " + type.PadRight(5, ' '));
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("------ПО ТИПУ------");
+            Console.WriteLine("Дата | Кол-во | Тип");
+            Console.WriteLine("");
+
+            for (int i = 0; i < size; i++)
+            {
+                if (Convert.ToString(data_rw[i, 3]) == "дождь")
+                {
+                    string day = Convert.ToString(data_rw[i, 0]);
+                    string mounth = Convert.ToString(data_rw[i, 1]);
+                    string count = Convert.ToString(data_rw[i, 2]);
+                    string type = Convert.ToString(data_rw[i, 3]);
+
+                    Console.WriteLine(day + "." + mounth + "  |  " + count.PadRight(5, ' ') + " | " + type.PadRight(5, ' '));
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("------ПО КОЛ-ВУ-----");
+            Console.WriteLine("Дата | Кол-во | Тип");
+            Console.WriteLine("");
+
+            for (int i = 0; i < size; i++)
+            {
+                if (Convert.ToDouble(data_rw[i, 2]) <= 1.5)
+                {
+                    string day = Convert.ToString(data_rw[i, 0]);
+                    string mounth = Convert.ToString(data_rw[i, 1]);
+                    string count = Convert.ToString(data_rw[i, 2]);
+                    string type = Convert.ToString(data_rw[i, 3]);
+
+                    Console.WriteLine(day + "." + mounth + "  |  " + count.PadRight(5, ' ') + " | " + type.PadRight(5, ' '));
+                }
             }
 
             Console.ReadKey();
