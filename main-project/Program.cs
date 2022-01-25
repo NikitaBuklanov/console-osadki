535c7fe2e00bdfbf09fee63c6a992267b5f1984f
NikitaBuklanov <buklanovnik@gmail.com>
2022-01-25 16:34:10 +0300

    Создана заготовка проекта

---
+++ main-project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello\n");
+            Console.ReadKey();
+        }
+    }
+}
