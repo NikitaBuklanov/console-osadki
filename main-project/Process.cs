7775a04f8d5e2b54b4a2c979d1bf4c3b212338f6
NikitaBuklanov <buklanovnik@gmail.com>
2022-01-25 18:24:26 +0300

    Тест1

--- main-project/Process.cs
+++ main-project/Process.cs
@@ -8,5 +8,18 @@ namespace main_project
 {
     class Process
     {
+        public double ObKol(int mounth, double[,] arr)
+        {
+            int n = arr.GetLength(0);
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i, 0] == mounth)
+                {
+                    sum += arr[i, 1];
+                }
+            }
+            return sum;
+        }
     }
 }
---
+++ unit-test-project/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("unit-test-project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unit-test-project")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05b81ada-5c75-41bb-8108-8f5a11813409")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
